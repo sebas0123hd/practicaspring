fc60c9066d83a53c31efc62482bccba0ef56c51e
Martin Sebastian Ramos Espinoza <martin_sebastian@comunidad.unam.mx>
2025-04-22 21:06:42 -0600

    Proyecto

---
+++ practicaspring/src/main/java/com/daw/practicaspring/PracticaspringApplication.java
@@ -0,0 +1,13 @@
+package com.daw.practicaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaspringApplication.class, args);
+	}
+
+}
